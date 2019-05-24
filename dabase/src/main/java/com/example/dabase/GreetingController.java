bc1f3f5a3d55c9dfcf914381c4bf8353b5ebef98
SevenEleven01 <50944656+SevenEleven01@users.noreply.github.com>
2019-05-24 16:55:54 +0300

    Add files via upload

--- dabase/src/main/java/com/example/dabase/GreetingController.java
+++ dabase/src/main/java/com/example/dabase/GreetingController.java
@@ -1,13 +1,22 @@
 package com.example.dabase;
 
+import com.example.dabase.domain.Message;
+import com.example.dabase.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import java.security.MessageDigest;
+import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -15,9 +24,33 @@ public class GreetingController {
         model.put("name", name);
         return "greeting";
     }
+
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Love me!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, @RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter !=null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
         return "main";
     }
 }
