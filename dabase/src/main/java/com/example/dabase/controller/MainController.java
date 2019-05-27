117ad7f300b16c2c9d56aa09439e02d9bcfe536e
SevenEleven01 <50944656+SevenEleven01@users.noreply.github.com>
2019-05-27 10:54:38 +0300

    Add files via upload

---
+++ dabase/src/main/java/com/example/dabase/controller/MainController.java
@@ -0,0 +1,54 @@
+package com.example.dabase.controller;
+
+import com.example.dabase.domain.Message;
+import com.example.dabase.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.security.MessageDigest;
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, @RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter !=null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+        return "main";
+    }
+}
---
+++ dabase/src/main/java/com/example/dabase/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.dabase.domain;
+
+public enum Role {
+    USER;
+}
