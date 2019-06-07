d57e7feb2b6aa8a97e38b105a435d19b1547d101
SevenEleven01 <50944656+SevenEleven01@users.noreply.github.com>
2019-06-07 16:00:30 +0300

    Add files via upload

--- dabase/src/main/java/com/example/dabase/domain/Message.java
+++ dabase/src/main/java/com/example/dabase/domain/Message.java
@@ -1,13 +1,20 @@
 package com.example.dabase.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Please fill in the message")
+    @Length(max = 2048, message = "Message is too long")
     private String text;
+    @Length(max = 255, message = "Tag is too long")
     private String tag;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
